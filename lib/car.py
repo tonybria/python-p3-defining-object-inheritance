7b36f2beb1d021e06685f2439d778857682e4d4e
tony-brian <btoni3823@gmail.com>
2023-08-27 22:14:32 +0300

    completed assigment

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,13 @@
 from vehicle import Vehicle
 
-class Car(Vehicle):
-    pass
+class Car:
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+class Car(Vehicle):  # Assuming Car is a subclass of Vehicle
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+# Now you can create instances of the Car class
+my_car = Car(48, 4)
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,13 @@
 class Vehicle:
-    pass
+
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def fill_up_tank(self):
+        return "filling up!"
+
+# Now you can create instances of the Vehicle class
+my_vehicle = Vehicle(48, 4)
+
+
